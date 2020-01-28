663c666ebafa257d782a54e56bd830a91507d5e4
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-01-28 17:59:43 +0530

    host ip

--- resources/template-2/nginx-install.sh
+++ resources/template-2/nginx-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update -y
 apt install -y nginx
-echo "Hello World from updated host" $HOSTNAME "!" | sudo tee /var/www/html/index.html
+echo "Hello World from updated host $HOSTNAME and IP  `hostname -I` !" | sudo tee /var/www/html/index.html
